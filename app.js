471707d9d86aeb8d9bd1c139f143c983806cbbb6
Brett Starks <brettz.starks@gmail.com>
2019-03-13 20:35:42 -0600

    Adding initial files

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-const connectionString = process.env.DATABASE_URL;
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const connectionString = process.env.DATABASE_URL;
+
+const {Pool, Client} = require('pg');
+const pool = new Pool({connectionString: connectionString});
+const client = new Client({connectionString: connectionString});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  query = "SELECT * FROM ranks";
+
+  pool.query(query, function(err, result) {
+    if(err) {
+      console.log("ERROR: " + err);
+    } else {
+      res.write(JSON.stringify(result.rows))
+      res.end();
+    }
+    pool.end();
+
+  })
+
   res.render('index', { title: 'Express' });
 });
 
